87febfc0f2af2a083d6e7030803b760b72a414d7
aryan1908 <aryan@webexpertinfotech.com>
2025-10-27 15:09:15 +0530

    Add logout functionality using new branch

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,14 +22,13 @@
 
     <div class="container">
 
-        <!-- Outer Row -->
         <div class="row justify-content-center">
 
             <div class="col-xl-10 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
+
 @if ($errors->any())
     <div class="alert alert-danger">
         <ul class="mb-0">
